2dcbd5dea905e60f9e59bb37705ab580fb78215a
DoraLC <34760377+DoraLC@users.noreply.github.com>
2018-01-23 15:43:40 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -4,29 +4,21 @@ enum d {
     //% block="Backward"
     B
 }
-enum leds {
-    //% block="On"
-    on = 1,
-    //% block="Off"
-    off = 0
-}
 
-//% weight=0 color=#2E2EFE icon="\uf02d" block="Learning Packs"
+//% weight=0 color=#2E2EFE icon="\uf1b9" block="Learning Packs"
 namespace LearningPack {
     //% blockId="pack1" block="Pack 1 LED |red %red|yellow %yellow|green %green"
     //% blockGap=2 weight=0 blockExternalInputs=true
     //% red.min=0 red.max=1 yellow.min=0 yellow.max=1 green.min=0 green.max=1
-    //% icon="\uf0eb"
-    export function LED(red: leds, yellow: leds, green: leds): void {
+    export function LED(red: number, yellow: number, green:number): void{
         pins.digitalWritePin(DigitalPin.P0, red)
         pins.digitalWritePin(DigitalPin.P1, yellow)
         pins.digitalWritePin(DigitalPin.P2, green)
     }
-
+    
     //% blockId="pack3" block="Pack 3 car speed |left %left|right %right|direction %d"
     //% blockGap=2 weight=1 blockExternalInputs=true
     //% left.min=0 left.max=1023 right.min=0 right.max=1023
-    //% icon="\uf1b9"
     export function SpeedControl(left: number, right: number, Direction: d): void {
         if (Direction == d.F) {
             pins.digitalWritePin(DigitalPin.P12, 0)
