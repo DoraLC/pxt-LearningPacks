3df3e32cc7f0e49cd00d0b564e256e050f2547a4
DoraLC <34760377+DoraLC@users.noreply.github.com>
2018-01-26 16:21:09 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,24 @@
 enum d {
     //% block="Forward"
-    F=0,
+    F = 0,
     //% block="Backward"
-    B=1
+    B = 1
 }
 enum leds {
     //% block="On"
-    on=1, 
+    on = 1,
     //% block="Off"
-    off=0
+    off = 0
+}
+enum Gate {
+    //% block="Open"
+    open = 90, 
+    //% block="Closed"
+    closed = 0
 }
 
 //% weight=0 color=#2E2EFE icon="\uf1b9" block="Learning Packs"
-namespace LearningPack{
+namespace LearningPack {
     //% blockId="LED" block="Pack 1 LED |red %red|yellow %yellow|green %green"
     //% blockGap=2 weight=0 blockExternalInputs=true
     export function LED(red: leds, yellow: leds, green: leds): void {
@@ -21,13 +27,19 @@ namespace LearningPack{
         pins.digitalWritePin(DigitalPin.P2, green)
     }
 
+    //% blockId="gate" block="Pack 2 Auto gate" Gate %gate
+    //% blockGap=2 weight=1
+    export function AG(gate: Gate): void {
+        pins.servoWritePin(AnalogPin.P1, gate)
+    }
+
     //% blockId="SpeedControl" block="Pack 3 car speed |left %left|right %right|direction %d"
-    //% blockGap=2 weight=1 blockExternalInputs=true
+    //% blockGap=2 weight=2 blockExternalInputs=true
     //% left.min=0 left.max=1023 right.min=0 right.max=1023
     export function SpeedControl(left: number, right: number, Direction: d): void {
-            pins.digitalWritePin(DigitalPin.P12, Direction)
-            pins.digitalWritePin(DigitalPin.P16, Direction)
-            pins.analogWritePin(AnalogPin.P8, left)
-            pins.analogWritePin(AnalogPin.P0, right)
+        pins.digitalWritePin(DigitalPin.P12, Direction)
+        pins.digitalWritePin(DigitalPin.P16, Direction)
+        pins.analogWritePin(AnalogPin.P8, left)
+        pins.analogWritePin(AnalogPin.P0, right)
     }
 }
